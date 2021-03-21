1c78795f1e7f40ee06ff2d2a5edcb9c80b59f8b7
fawsian <60254863+fawsian@users.noreply.github.com>
2021-03-21 17:54:32 +0100

    Create FrogJmp.java

---
+++ FrogJmp.java
@@ -0,0 +1,25 @@
+/*
+* @author     Fawzi Aiboud Nygren 2021
+* @return     amount of times frog has to jump
+*/
+
+class Solution {
+    public int solution(int X, int Y, int D) {
+        
+        int counter = 0;
+        int distance = Y-X;
+
+        if (X == Y) {
+            return 0;
+        }
+
+        if (distance%D == 0) {
+            return distance/D;
+        } else if (distance%D < D){
+            return distance/D + 1;
+        }
+        // Should never occur
+        return 0;
+
+    }
+}
